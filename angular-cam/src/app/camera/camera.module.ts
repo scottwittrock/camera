9a67584b957f48a3dcd5544f3625506e611e67e7
Scott Wittrock <scott.witt.awp@gmail.com>
2019-08-22 16:24:49 -0400

    Removing test component

--- angular-cam/src/app/camera/camera.module.ts
+++ angular-cam/src/app/camera/camera.module.ts
@@ -4,18 +4,16 @@ import { CommonModule } from '@angular/common';
 import { CameraRoutingModule } from './camera-routing.module';
 import { CameraComponent } from './camera/camera.component';
 import { HttpClientModule } from '@angular/common/http';
-import { PreviewBigComponent } from './preview-big/preview-big.component';
 
 @NgModule({
-    declarations: [CameraComponent, PreviewBigComponent],
+    declarations: [CameraComponent ],
     imports: [
         CommonModule,
         CameraRoutingModule,
         HttpClientModule
     ],
     exports: [
-        CameraComponent,
-        PreviewBigComponent
+        CameraComponent
     ]
 })
 export class CameraModule { }
